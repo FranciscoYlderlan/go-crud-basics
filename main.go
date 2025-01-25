098c2e57ea1615404359989dadbc7d36a9fd8209
franciscoylderlan <ylderlan@nca.ufma.br>
2025-01-25 20:55:49 -0300

    :adhesive_bandage: fix: register route in mux and fix headers order

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func (c *ContactStore) Create(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&newContact)
 
 	if err != nil {
+		log.Println("Error decoding JSON:", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -35,23 +36,40 @@ func (c *ContactStore) Create(w http.ResponseWriter, r *http.Request) {
 
 	c.Contacts[id] = newContact
 
-	w.Header().Set("Context-Type", "application/json")
-
-	json.NewEncoder(w).Encode(newContact)
+	w.Header().Set("Content-Type", "application/json")
 
 	w.WriteHeader(http.StatusCreated)
+
+	err = json.NewEncoder(w).Encode(newContact)
+	if err != nil {
+		log.Println("Error encoding JSON:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("Contact created:", newContact)
 
 }
 
 func main() {
 
-	// service := &ContactStore{Contacts: make(map[int]Contact)}
+	service := &ContactStore{Contacts: make(map[int]Contact)}
 
 	mux := http.NewServeMux()
 
-	http.HandleFunc("/contacts", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	mux.HandleFunc("/contacts", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL)
+		switch r.Method {
+		case http.MethodGet:
+			fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		case http.MethodPost:
+			service.Create(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+
 	})
+	log.Println("Server started on http://localhost:8080")
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
 
