0a387bade4e79652f68d3b8cd4ed0ea7aa0f5ba1
franciscoylderlan <ylderlan@nca.ufma.br>
2025-01-25 16:11:07 -0300

    :zap: refact: replace DefaultServeMux with ServeMux

--- main.go
+++ main.go
@@ -8,10 +8,18 @@ import (
 
 func main() {
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
+	// helloHandler := func(w http.ResponseWriter, req *http.Request) {
+	// 	io.WriteString(w, "Hello, world!\n")
+	// }
 
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// http.HandleFunc("/hello", helloHandler)
+
+	mux := http.NewServeMux()
+
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
+
 }
